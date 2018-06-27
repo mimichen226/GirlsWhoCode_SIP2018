86644418ebb71b3e0c8d03208f0a2db2defef025
Mimi Chen <mimichen226@gmail.com>
2018-06-26 23:32:34 -0700

    Start code to duplicates

---
+++ Python/START_sort_duplicates.py
@@ -0,0 +1,15 @@
+# Declare variables
+mylist = [2,3,1,3,5]   # Our inputs to the program
+has_duplicates = False # initialize as false (why?)
+
+# TODO: Sort 'myList' (Why do we sort first?)
+
+# TODO: Loop through 'mylist' with a for-Loop
+
+    # TODO: Check if adjacent elements of 'mylist' are the same
+
+        # TODO: if they are the same, set has_duplicates to True
+
+        # TODO: Exit out of the for-loop (no need to check rest of list)
+
+print(has_duplicates) # Our outputs of the program

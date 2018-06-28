08e0367d6f0945eaa08e1c6bafd5351e15e4adcf
Mimi Chen <mimichen226@gmail.com>
2018-06-28 00:33:59 -0700

    Start code for rock paper scissors

---
+++ Python/W2D4_Libraries/START_rock_paper_scissors.py
@@ -0,0 +1,23 @@
+########### Starter code for Rock Paper Scissors ############
+
+# TODO: Import the module (aka library) random
+
+# TODO: Create a list of possible moves in rock paper scissors
+gestures = ["TODO", "TODO", "TODO"]
+
+# Allow the computer to make a random selection on the move
+computer = random.choice(gestures)
+
+# TODO: Take in user input for rock, paper, or scissors
+# BE SURE TO PROCESS INPUT (valid inputs allowed ONLY)
+human = # TODO
+
+# Show the human player what the computer decided on
+print("Computer chooses {}".format(computer.upper()))
+
+# Determine who wins
+# TODO: when would there be a tie?
+
+# TODO: when does the computer win?
+
+# TODO: when does the human win?

1c5c7fbceaa8786941fd5df70c57cf49a05681b7
Akshita Nathani <anathani1998@gmail.com>
2018-06-28 09:35:36 -0700

    Play games UI

---
+++ Python/W2D4_Libraries/START_playgames.py
@@ -0,0 +1,23 @@
+#TODO: import your games library into this folder
+
+#TODO: create a list of available games
+
+#TODO: create a function to display the list of available available games
+    #HINT: use a loop
+
+#TODO: write a function to evaluate the player's choice of game
+    #TODO: ask the player which game they wish to play
+    #TODO: run that particular game by calling the respective game with the dot notation
+
+#TODO: define a main function to run the games
+
+    #TODO: call the function that displays a list of available available_games
+
+    #TODO: call the function that evaluates the player's choice of game
+
+    #TODO: ask the player if they wish to keep playing
+
+    #TODO: use a loop to keep playing if the player answers yes
+
+
+#TODO: call the main function appropriately

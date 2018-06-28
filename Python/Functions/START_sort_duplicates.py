42af094cd8590ede36586e83b0d4a0ddbb8e198e
Mimi Chen <mimichen226@gmail.com>
2018-06-27 23:41:06 -0700

    Added files

--- Python/Functions/START_sort_duplicates.py
+++ Python/Functions/START_sort_duplicates.py
@@ -2,14 +2,14 @@
 mylist = [2,3,1,3,5]   # Our inputs to the program
 has_duplicates = False # initialize as false (why?)
 
-# TODO: Sort 'myList' (Why do we sort first?)
-
+# Sort 'myList' (Why do we sort first?)
+mylist.sort()
 # TODO: Loop through 'mylist' with a for-Loop
-
-    # TODO: Check if adjacent elements of 'mylist' are the same
-
-        # TODO: if they are the same, set has_duplicates to True
-
-        # TODO: Exit out of the for-loop (no need to check rest of list)
-
+for index in range(len(mylist) - 1): # 0, 1, 2, 3
+    # Check if adjacent elements of 'mylist' are the same
+    if mylist[index] == mylist[index + 1]:
+        # if they are the same, set has_duplicates to True
+        has_duplicates = True
+        # Exit out of the for-loop (no need to check rest of list)
+        break
 print(has_duplicates) # Our outputs of the program
--- Python/Lists/hangman.py
+++ Python/Lists/hangman.py
@@ -1,52 +1,57 @@
-# Have a word/ phrase
-phrase = "turtles are cool"
+# defining the function display
+def print_beginning_of_game():
+    print("WELCOME TO HANGMAN")
 
-# Keep track of user's guesses
-guessed_letters = []
-game_over = False
-
-# Tell user how many spaces and letters they need to guess
-display = ""
-for letter in phrase:
-    if letter in guessed_letters:
-        display += letter
-    # Processing the space in our phrase
-    elif letter == " ":
-        display += "  "
-    # the letter in our phrase has not been guessed yet
-    else:
-        display += "_ "
-print(display)
-
-while game_over != True:
-    # Allow user to give input to computer
-    guess = input("Enter a letter: ")
-
-    # Tell the user if they get the right letter
-    if guess in phrase:
-        print("You got a letter: {}".format(guess))
-    # Tell the user if they get the wrong letter
-    else:
-        print("{} is not in the phrase".format(guess))
-    # Add the guess letter to our list of guessed letters
-    guessed_letters.append(guess)
-
-    # Tell user how many spaces and letters they need to guess
-    display = ""
-    for letter in phrase:
-        if letter in guessed_letters:
-            display += letter
+def display(word, already_guessed_letters):
+    new_str = ""
+    for letter in word:
+        if letter in already_guessed_letters:
+            new_str += letter
         # Processing the space in our phrase
         elif letter == " ":
-            display += "  "
+            new_str += "  "
         # the letter in our phrase has not been guessed yet
         else:
-            display += "_ "
-    print(display)
+            new_str += "_ "
+    return new_str
 
-    # End the game if the user gets all the right letters in our word/ phrase
-    if "_" in display:
-        game_over = False
-    else:
-        game_over = True
-print("END OF GAME")
+def main():
+    print_beginning_of_game()
+
+    # Have a word/ phrase
+    phrase = "turtles are cool"
+
+    # Keep track of user's guesses
+    guessed_letters = []
+    game_over = False
+
+    # Tell user how many spaces and letters they need to guess
+    beginning = display(phrase, guessed_letters) # Calling function display
+    print(beginning)
+
+    while game_over != True:
+        # Allow user to give input to computer
+        guess = input("Enter a letter: ")
+
+        # Tell the user if they get the right letter
+        if guess in phrase:
+            print("You got a letter: {}".format(guess))
+        # Tell the user if they get the wrong letter
+        else:
+            print("{} is not in the phrase".format(guess))
+        # Add the guess letter to our list of guessed letters
+        guessed_letters.append(guess)
+
+        # Tell user how many spaces and letters they need to guess
+        wakeup = display(phrase, guessed_letters)
+        print(wakeup)
+
+        # End the game if the user gets all the right letters in our word/ phrase
+        if "_" in wakeup:
+            game_over = False
+        else:
+            game_over = True
+    print("END OF GAME")
+
+if __name__ == "__main__":
+    main()
